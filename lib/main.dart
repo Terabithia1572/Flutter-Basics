83dce4b097463dafa5ee38243ce2819c52902f6e
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2026-02-03 21:05:59 +0300

    Tutorials #12 Container / Padding / Margin / Card / SizedBox / Align Widget- Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Uygulamam());
+  runApp(const Uygulamam());
 }
 
 class Uygulamam extends StatelessWidget {
@@ -14,7 +14,7 @@ class Uygulamam extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
-          title: Text(
+          title: const Text(
             'Uygulamam',
             style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
           ),
@@ -23,30 +23,39 @@ class Uygulamam extends StatelessWidget {
         ),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
-                onPressed: () => print('Bana Tıkladın'),
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.indigo),
-                child: Text(
-                  'Bana Tıkla',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.amber,
-                  ),
+              Container(
+                padding: const EdgeInsets.only(top: 20),
+                margin: const EdgeInsets.all(20),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade300,
+                      blurRadius: 6,
+                      spreadRadius: 1,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: Text('Ruwis', style: const TextStyle(fontSize: 24)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 200),
+                child: Image.asset(
+                  'assets/images/terabithiaremote1.png',
+                  height: 100,
                 ),
               ),
-              TextButton(
-                onPressed: () {},
-                child: Text('Bana Tıkla'),
-                style: TextButton.styleFrom(backgroundColor: Colors.amber),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.arrow_forward_ios,
-                  color: Colors.red,
-                  size: 40,
+              const SizedBox(
+                width: 200,
+                height: 80,
+                child: Card(
+                  color: Colors.amber,
+                  elevation: 5,
+                  child: Align(child: Text('Merhaba')),
                 ),
               ),
             ],
