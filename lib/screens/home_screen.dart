5bba117331cfcd90c819635b8a4bc7b33eed5604
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2026-02-05 22:40:51 +0300

    16# Flutter Tutorial - Button & Gesture Detector

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,24 +13,49 @@ class HomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Stack(
+              alignment: Alignment.center,
               children: [
                 Image.asset('assets/images/zikirmatik.png', width: 300),
-                Positioned(
-                  top: 46,
-                  right: 80,
-                  child: Text(
-                    '50',
-                    style: TextStyle(
-                      fontFamily: 'Digital7',
-                      fontSize: 50,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
+                Positioned(top: 46, right: 80, child: _counterText()),
+                Positioned(bottom: 30, child: _incrementButton()),
+                Positioned(right: 76, bottom: 114, child: _resetButton()),
               ],
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  GestureDetector _resetButton() {
+    return GestureDetector(
+      onTap: () => print('Reset'),
+      child: Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(60)),
+      ),
+    );
+  }
+
+  GestureDetector _incrementButton() {
+    return GestureDetector(
+      onTap: () => print('Tıklandı'),
+      child: Container(
+        width: 90,
+        height: 90,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(60)),
+      ),
+    );
+  }
+
+  Text _counterText() {
+    return Text(
+      '50',
+      style: TextStyle(
+        fontFamily: 'Digital7',
+        fontSize: 50,
+        color: Colors.white,
       ),
     );
   }
